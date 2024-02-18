bd65b99722b356067468c9c39406f867e8c5c77f
enibalo <abibalo@hotmail.com>
2024-02-18 00:37:19 -0700

    csv search

--- SearchFunctionAddress.py
+++ SearchFunctionAddress.py
@@ -1,32 +1,27 @@
 import csv
+import pandas as pd
 
-def searchFunctionAddress(file_path, column1_value, column2_value, column1_index, column2_index):
+def searchFunctionAddress(file_url, column1_value, column2_value, column1_index, column2_index):
     matching_rows = []
 
-    with open(file_path, 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        header = next(reader)  # Skip the header row
+    # Read the CSV file from the GitHub URL using Pandas
+    df = pd.read_csv(file_url)
 
-        for row in reader:
-            print(row[column1_index])
-            # Check if both column1_value and column2_value exist in the specified columns
-            if row[column1_index] == column1_value and row[column2_index] == column2_value:
-                print(row[column1_index], row[column2_index])
-                matching_rows.append(row)
+    matching_rows = df[(df[column1_name] == column1_value) & (df[column2_name] == column2_value)]
 
     return matching_rows
 
 # Example usage:
-file_path = 'C:/Users/ayesh/OneDrive/Desktop/Hackathon Project/Dataset_for_map_app_divided1.csv'  # Path to your CSV file
+github_file_url = 'https://raw.githubusercontent.com/KhadeejaAbbas/CalgaryHacks2024/main/Dataset_for_map_app_divided.csv?token=GHSAT0AAAAAACN7XUSWBCKNO6ERCL3JOKJ2ZORXXVQ'  # URL of the CSV file on GitHub
+
 column1_value = '12 Av SW '  # First search string
 column2_value = '8 St SW '  # Second search string
-column1_index = 10  # Index of the first column to search in
-column2_index = 11  # Index of the second column to search in
+column1_name = 'First_address'  # Name of the first column to search in
+column2_name = 'Second_address'  # Name of the second column to search in
 
-matching_rows = searchFunctionAddress(file_path, column1_value, column2_value, column1_index, column2_index)
-if matching_rows:
+matching_rows = searchFunctionAddress(github_file_url, column1_value, column2_value, column1_name, column2_name)
+if not matching_rows.empty:
     print("Matching rows found:")
-    for row in matching_rows:
-        print(row)
+    print(matching_rows)
 else:
     print("No matching rows found.")
