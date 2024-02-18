30bf3efc80a2aebb90d9309471a3fce56e52471d
enibalo <abibalo@hotmail.com>
2024-02-18 09:32:30 -0700

    change server

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, requests, StringIO
+from flask import Flask, render_template, request, redirect, url_for 
+import requests, StringIO
 import csv
 import pandas as pd
 
